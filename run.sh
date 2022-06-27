070883899ff94c5feb47f48492cba77b7b7fa22b
Thai Le <thaile@147dda275dde.ant.amazon.com>
2022-06-27 11:05:44 -0500

    updates

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 for layer1 in 10 20 30
+do
 	for layer2 in 5 10 15
+	do
 		for lr in 1e-5 1e-4 1e-3
+		do
 			echo "${layer1} ${layer2} ${lr}"
 			python main4.py --filename "news_categorization_reu.csv" --model_name "NN" --nn_layer1 ${layer1} --nn_layer2 ${layer2} --nn_lr ${lr}
-		do
-	do
-do
+		done
+	done
+done
